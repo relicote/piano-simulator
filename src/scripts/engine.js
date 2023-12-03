2cc4ca7c2091d024d42c843f56978c2d10539d47
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-12-03 12:22:14 -0300

    interpolando notas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,11 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
-let audio = new Audio("tunes");
-const playTune = (key) >{};
+let audio = new Audio("src/tunes/a.wav");
+
+
+const playTune = (key) =>{
+    audio.src = `src/tunes/${key}.wav`
+    audio.play();
+};
 
 pianoKeys.forEach((key) =>{
     console.log(key.dataset.key);
