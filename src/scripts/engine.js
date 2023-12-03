b9924db550a6ebc6e66fd9db83961314b2bf5ca1
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-12-03 12:39:35 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,6 +2,8 @@ const pianoKeys = document.querySelectorAll('.piano-keys .key');
 let audio = new Audio("src/tunes/a.wav");
 
 
+let mapedKeys = [];
+
 const playTune = (key) =>{
     audio.src = `src/tunes/${key}.wav`
     audio.play();
@@ -16,9 +18,13 @@ const playTune = (key) =>{
 
 pianoKeys.forEach((key) =>{
     console.log(key.dataset.key);
-    key.addEventListener("click",()=> playTune(key.dataset.key))
+    key.addEventListener("click",()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key)
+    if (mapedKeys.includes(e.key)){
+        playTune(e.key)
+
+    }
 });
