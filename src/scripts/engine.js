697ce4ca129b13b16f4dd59742ecbfab77ad3f94
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-12-03 12:18:48 -0300

    att folder tunes

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,8 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
+let audio = new Audio("tunes");
+const playTune = (key) >{};
 
 pianoKeys.forEach((key) =>{
     console.log(key.dataset.key);
+    key.addEventListener("click",()=> playTune(key.dataset.key))
 })
